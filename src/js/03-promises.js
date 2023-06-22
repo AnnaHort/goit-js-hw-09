0ec42f61c86c7393f3dcddeaae12f2ec8aed130e
AnnaHort <annahort3d@gmail.com>
2023-06-22 21:46:13 +0300

    finished task - 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const handlePromise = (e) => {
+  e.preventDefault(); // Забороняємо перезавантаження сторінки
+
+  // Отримуємо значення полів форми
+  const delay = parseInt(delayEl.value);
+  const step = parseInt(stepEl.value);
+  const amount = parseInt(amountEl.value);
+
+  // Запускаємо цикл для створення промісів
+  for (let i = 1; i <= amount; i += 1) {
+    const position = i;
+    const promiseDelay = delay + (i - 1) * step;
+
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+formEl.addEventListener('submit', handlePromise);
+
+
+
